57fad21a0123c4ae7c3851dcd1adf78b0587bba7
as14df <alessa.seeger@web.de>
2019-03-25 13:56:06 +0100

    added new folder for Translator app

---
+++ Translator/translate/speaker/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+
+# TODO: Hier können SIe eine Klasse anlegen, die Ihr Modell erzeugt
+
+class  xxx(models.Model):
+    """A typical class defining a model, derived from the Model class."""
+
+    # Fields
+    # TODO Fügen Sie hier Ihre Felder ein 
+
+    # Metadata
+    #class Meta: 
+    #    ordering = ['-my_field_name']
+
+    # Methods
+    #def get_absolute_url(self):
+    #    """Returns the url to access a particular instance of MyModelName."""
+    #    return reverse('model-detail-view', args=[str(self.id)])
+    
+    #def __str__(self):
+    #    """String for representing the MyModelName object (in Admin site etc.)."""
+    #    return self.my_field_name
---
+++ Translator/translate/speaker/views.py
@@ -0,0 +1,16 @@
+# Views
+
+from django.shortcuts import render
+from django.template import loader
+from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+
+from .models import dict_ge_en
+from .forms import *
+
+# TODO Impord er Code Snippets für die Google Translation
+from .google_translate_snippets import *
+
+# def index(request):
+#     return HttpResponse("Hello, world. You're at the speaker index.")
+
